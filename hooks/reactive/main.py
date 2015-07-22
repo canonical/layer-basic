b1a6593963dd4f9e3cf3b2de798f6bc8be5f0df3
Benjamin Saller <bcsaller@gmail.com>
2015-07-21 20:33:20 -0700

    remove apt_purge, unit teardown should be fine

--- hooks/reactive/main.py
+++ hooks/reactive/main.py
@@ -5,7 +5,6 @@ from charmhelpers.core.reactive import hook
 from charmhelpers.core.unitdata import kv
 
 from charmhelpers.fetch import apt_install
-from charmhelpers.fetch import apt_purge
 from charmhelpers.fetch import filter_installed_packages
 
 
@@ -18,11 +17,3 @@ def install_packages():
         status_set('maintenance', 'Installing packages')
         apt_install(filter_installed_packages(local.PACKAGES))
         db.set('installed', True)
-
-
-@hook('stop')
-def uninstall_packages():
-    if db.get('installed'):
-        status_set('maintenance', 'Purging packages')
-        apt_purge(local.PACKAGES)
-        db.unset('installed')
