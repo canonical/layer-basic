2750eec8be377eeee70566977adddbd2dddad83a
Benjamin Saller <bcsaller@gmail.com>
2015-08-21 09:17:49 -0700

    patches from cory

--- reactive/main.py
+++ reactive/main.py
@@ -1,11 +1,14 @@
-from reactive import local
-
 from charmhelpers.core.hookenv import status_set
-from charmhelpers.core.reactive import hook
 from charmhelpers.core.unitdata import kv
-
 from charmhelpers.fetch import apt_install
 from charmhelpers.fetch import filter_installed_packages
+
+from charms.reactive import hook
+
+try:
+    from local import PACKAGES
+except ImportError:
+    PACKAGES = []
 
 
 db = kv()
@@ -13,7 +16,7 @@ db = kv()
 
 @hook('install')
 def install_packages():
-    if not db.get('installed'):
+    if PACKAGES and not db.get('installed'):
         status_set('maintenance', 'Installing packages')
-        apt_install(filter_installed_packages(local.PACKAGES))
+        apt_install(filter_installed_packages(PACKAGES))
         db.set('installed', True)
