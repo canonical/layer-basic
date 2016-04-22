e8ffdbde7453fe82ec8d6e911540e6c40affdddd
Cory Johns <johnsca@gmail.com>
2016-04-22 13:46:04 -0400

    Re-apply default state commit, with fix

--- lib/charms/layer/basic.py
+++ lib/charms/layer/basic.py
@@ -119,15 +119,25 @@ def apt_install(packages):
 
 
 def init_config_states():
+    import yaml
     from charmhelpers.core import hookenv
     from charms.reactive import set_state
     from charms.reactive import toggle_state
     config = hookenv.config()
+    config_defaults = {}
+    config_yaml = os.path.join(hookenv.charm_dir(), 'config.yaml')
+    if os.path.exists(config_yaml):
+        with open(config_yaml) as fp:
+            config_defs = yaml.load(fp).get('options', {})
+            config_defaults = {key: value.get('default')
+                               for key, value in config_defs.items()}
     for opt in config.keys():
         if config.changed(opt):
             set_state('config.changed')
             set_state('config.changed.{}'.format(opt))
         toggle_state('config.set.{}'.format(opt), config[opt])
+        toggle_state('config.default.{}'.format(opt),
+                     config[opt] == config_defaults[opt])
     hookenv.atexit(clear_config_states)
 
 
@@ -139,4 +149,5 @@ def clear_config_states():
     for opt in config.keys():
         remove_state('config.changed.{}'.format(opt))
         remove_state('config.set.{}'.format(opt))
+        remove_state('config.default.{}'.format(opt))
     unitdata.kv().flush()
