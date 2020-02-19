f06bbc4d4fb45e5a20774ad841854eaac9cb7650
Cory Johns <johnsca@gmail.com>
2020-02-19 14:09:11 -0500

    Fix line-too-long lint error.

--- lib/charms/layer/basic.py
+++ lib/charms/layer/basic.py
@@ -146,7 +146,8 @@ def bootstrap_charm_deps():
         if cfg.get('use_venv'):
             if not os.path.exists(venv):
                 series = get_series()
-                if series in ('ubuntu12.04', 'precise', 'ubuntu14.04', 'trusty'):
+                if series in ('ubuntu12.04', 'precise',
+                              'ubuntu14.04', 'trusty'):
                     apt_install(['python-virtualenv'])
                 elif 'centos' in series:
                     yum_install(['python-virtualenv'])
